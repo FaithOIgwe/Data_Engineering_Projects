3136e543d6c487971b305a3a2f4e49f387eb436f
Faith Igwe <108904370+FaithOIgwe@users.noreply.github.com>
2026-09-07 20:10:43 +0100

    Update python for Data Engineering.py

--- python for Data Engineering.py
+++ python for Data Engineering.py
@@ -24,4 +24,37 @@ for item in loan_amounts:
     else:
         print(item, "Small")
 
+#Practice 4 (Count how many loans are greater than 10,000.)
+#loan_amounts = [5000, 10000, 15000, 20000]
+large_loan_count = 0
+
+for item in loan_amounts:
+    if item > 10000:
+        large_loan_count += 1
+
+print(large_loan_count)
+
+#Calculate the total of all the loan amounts without using sum():
+loan_amounts = [5000, 10000, 15000, 20000]
+
+total_amount = 0
+
+for item in loan_amounts:
+    total_amount += item
+
+print(total_amount)
+
+#Calculate the total of only the loan amounts greater than 10,000.
+loan_amounts = [5000, 10000, 15000, 20000]
+
+large_loan_total = 0
+
+for item in loan_amounts:
+    if item > 10000:
+        large_loan_total += item
+
+print(large_loan_total)
+
+
+
 
