874f34bb89d3a991c3a39aeebf707afe76071b1e
Faith Igwe <108904370+FaithOIgwe@users.noreply.github.com>
2026-09-15 19:54:18 +0100

    Update Data Imports in Python.py

--- Data Imports in Python.py
+++ Data Imports in Python.py
@@ -8,6 +8,7 @@ print(loans.head())
 print("Dataset size:", loans.shape)
 print("Column names:", loans.columns)
 
+#importing json files
 import pandas as pd
 
 borrowers = pd.read_json("borrower_profiles.json")
@@ -16,7 +17,7 @@ print(borrowers.head())
 print("Dataset size:", borrowers.shape)
 print("Column names:", borrowers.columns)
 
-
+#viewing data shape
 import pandas as pd
 
 loans = pd.read_csv("loan_applications.csv")
@@ -30,3 +31,5 @@ print(borrowers.head())
 
 print("\nLoans size:", loans.shape)
 print("Borrowers size:", borrowers.shape)
+
+
